5dfd6675098efb28c831be96ff87fca432cc7e95
Sylvain <s.metayer@groupeonepoint.com>
2025-02-03 15:44:33 +0100

    feat: prepare tnt

---
+++ qrCodes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+qrencode https://openfeedback.io/w3JorGi7ut6d9keUsCvO/2025-02-06/cm28x3w3s00dk6gvvmsyrb4mr -o images/openfeedback.svg -t SVG -l H -m 2
+qrencode https://r.sylvain.dev/tnt-2025 -o images/slides_link.svg -t SVG -l H -m 2
